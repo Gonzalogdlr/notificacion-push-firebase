e2edc7a39345db127b1fc91587070d233bfd64e4
Gonzalo <gologdlr@gmail.com>
2021-09-29 11:02:20 -0500

    ADD: Contansts and save token

--- App.js
+++ App.js
@@ -1,40 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useEffect, useState} from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import  Constants  from "expo-constants";
 
 export default function App() {
+  const [token, setToken] = useState("")
   registerForPushNotificationsAsync = async () => {
     if (Constants.isDevice) {
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
       let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
+      if (existingStatus !== 'granted') {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
         return;
       }
       const token = (await Notifications.getExpoPushTokenAsync()).data;
       console.log(token);
-      this.setState({ expoPushToken: token });
+      setToken({ expoPushToken: token });
     } else {
-      alert("Must use physical device for Push Notifications");
+      alert('Must use physical device for Push Notifications');
     }
-
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
         importance: Notifications.AndroidImportance.MAX,
         vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
+        lightColor: '#FF231F7C',
       });
     }
-  };
+    };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open usadadsap  your app!</Text>
+      <Pressable onPress={
+       registerForPushNotificationsAsync 
+      }>
+        <Text>Tokens</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
